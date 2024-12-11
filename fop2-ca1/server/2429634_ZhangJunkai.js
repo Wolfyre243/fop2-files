ff0473d08d3dc853b82b745c00966729ebb6f377
Wolfyre243 <wolfyreblueflare@gmail.com>
2024-12-11 22:51:07 +0800

    added question 2

--- fop2-ca1/server/2429634_ZhangJunkai.js
+++ fop2-ca1/server/2429634_ZhangJunkai.js
@@ -6,6 +6,7 @@
 
 // ---------------------------- Import Dependencies -----------------------------
 const input = require('readline-sync');
+const fetch = require('node-fetch');
 
 // --------------------------------- VARIABLES ----------------------------------
 
@@ -21,40 +22,117 @@ const menu =
     '7. Exit\n' +
     '>>> ';
 
+const endpointURL = 'http://localhost:8081';
 
 
 // ------------------------------ Helper Functions ------------------------------
+// Getters
+async function getAllRecords() {
+    // Return a promise that resolves to the json object sent by the server
+    return fetch(`${endpointURL}/allussuperstore`)
+        .then(async res => await res.json())
+        .catch(err => console.log(err));
+}
 
+// Calculation
+
+// Returns an object with the highest and lowest item in the given array
+const findHighLowByProperty = (arr, property) => { 
+    const sorted = arr.toSorted((a, b) => a[property] - b[property]);
+    return { lowest: sorted[0], highest: sorted[sorted.length - 1] };
+}
+
+// Gets the median of an object array by the specified property and returns the median object
+const findMedianByProperty = (arr, property) => arr.sort((a, b) => a[property] - b[property])[Math.floor(arr.length/2)];
 
 
 // ------------------------------- Main Functions -------------------------------
 
+async function displayFirstNRecords(n) {
+    // Format and log the data to the console.
+    return getAllRecords()
+        .then(res => {
+            console.log('Customer ID\tCustomer Name\t\tSegment\t\tState\t\t\tProduct Name\t\t\t\t\t\t\tSales (USD)');
+            res
+            .slice(0, n) // Get the first N records
+            // TODO: Fix formatting
+            .forEach(record => console.log(`${record.customerID}\t${record.customerName}\t\t${record.segment}\t${record.state} \t\t${record.productName}\t${record.sales}`))
+        })
+        .catch(err => console.log(err));
+}
 
+async function displayStatistics() {
+    // Initialise a Hashmap to store data
+    const segmentMap = new Map([
+        ['Consumer', 0],
+        ['Corporate', 0],
+        ['Home Office', 0]
+    ]);
 
+    const statArr = ['Lowest', 'Highest', 'Average', 'Median'];
+
+    return getAllRecords()
+        .then(res => {
+            // Set up data values and perform calculations
+            segmentMap.forEach((value, segment) => {
+                const filteredArr = res.filter(item => item.segment == segment);
+
+                const { lowest, highest } = findHighLowByProperty(filteredArr, 'sales');
+                const average = filteredArr.reduce((a, c) => a + c.sales, 0)/filteredArr.length;
+                const median = findMedianByProperty(filteredArr, 'sales');
+
+                segmentMap.set(segment, {
+                    lowest: lowest.sales,
+                    highest: highest.sales.toFixed(2),
+                    average: average.toFixed(2),
+                    median: median.sales.toFixed(2)
+                });
+            });
+
+            // Format and print the data
+            console.log('\t Consumer\t Corporate\t Home Office');
+            statArr.forEach(statistic => {
+                let finalStr = `${statistic}\t`;
+                segmentMap.forEach(data => finalStr = finalStr + ` $ ${data[statistic.toLowerCase()]}\t`);
+                console.log(finalStr);
+            });
+        })
+        .catch(err => console.log(err));
+}
 
 // ------------------------------ MENU (Main Loop) ------------------------------
 
-let userChoice = 0;
-while (userChoice != 7) {
-    // Prompt user for a choice
-    userChoice = input.questionInt(menu)
+async function Main() {
+    let userChoice = 0;
+    while (userChoice != 7) {
+        // Prompt user for a choice
+        userChoice = input.questionInt(menu);
 
-    if (userChoice == 1) {
-        console.log(userChoice); // Placeholder
-    } else if (userChoice == 2) {
-        console.log(userChoice);
-    } else if (userChoice == 3) {
-        console.log(userChoice);
-    } else if (userChoice == 4) {
-        console.log(userChoice);
-    } else if (userChoice == 5) {
-        console.log(userChoice);
-    } else if (userChoice == 6) {
-        console.log(userChoice);
-    } else if (userChoice == 7) {
-        console.log('Program exiting...');
-        break;
-    } else if (userChoice > 7) {
-        console.log('Invalid choice!');
-    }
-};
+        if (userChoice == 1) {
+            // 1. Display first 10 records of the Shopping Trend Data
+            console.log('US Superstore Data');
+            await displayFirstNRecords(10);
+
+        } else if (userChoice == 2) {
+            console.log('Lowest, highest, average and median sales by market segment.');
+            await displayStatistics();
+
+
+        } else if (userChoice == 3) {
+            console.log(userChoice);
+        } else if (userChoice == 4) {
+            console.log(userChoice);
+        } else if (userChoice == 5) {
+            console.log(userChoice);
+        } else if (userChoice == 6) {
+            console.log(userChoice);
+        } else if (userChoice == 7) {
+            console.log('Program exiting...');
+            break;
+        } else if (userChoice > 7) {
+            console.log('Invalid choice!');
+        }
+    };
+}
+
+Main();
---
+++ fop2-ca1/server/test.js
@@ -0,0 +1,16 @@
+const humanArr = [
+    {
+        id: 1,
+        name: 'peter'
+    },
+    {
+        id: 2,
+        name: 'peterpan'
+    },
+    {
+        id: 3,
+        name: 'peter3'
+    }
+]
+
+console.table(humanArr);
